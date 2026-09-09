29302243249e8c619c92d53ed9aba79a7c7c3487
Lucas Martinati <lucasm54800@gmail.com>
2026-09-09 18:38:06 +0200

    feat: ajout d'un temps de vie du cookie de la session (7j)

--- src/main.php
+++ src/main.php
@@ -8,6 +8,8 @@ error_reporting(E_ALL);
 
 require_once __DIR__ . '/vendor/autoload.php';
 
+// L'utilisateur reste connecté pendant 1 semaine
+session_set_cookie_params(60 * 60 * 24 * 7);
 session_start();
 
 DeefyRepository::setConfig(__DIR__ . '/config/deefy.db.ini');
