ce11f933294e7931fc1ee0e78020bde99f7b378d
Lucas Martinati <lucasm54800@gmail.com>
2026-09-13 18:33:46 +0200

    edit: css folder name

--- src/classes/dispatch/Dispatcher.php
+++ src/classes/dispatch/Dispatcher.php
@@ -52,7 +52,7 @@ class Dispatcher {
                     <title>Deefy</title>
                     <meta charset="utf-8">
                     <meta name="viewport" content="width=device-width, initial-scale=1">
-                    <link href="../style/style.css" rel="stylesheet">
+                    <link href="../css/style.css" rel="stylesheet">
                     <!-- css pour Bootstrap -->
                     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
                     <!-- css pour les icônes Boostrap -->
