c66fb1c219cf6e30da5e3b9d8056533426d05e7e
Lucas Martinati <lucasm54800@gmail.com>
2026-09-17 15:53:18 +0200

    feat: add fonts and improve css

--- src/classes/dispatch/Dispatcher.php
+++ src/classes/dispatch/Dispatcher.php
@@ -114,6 +114,10 @@ class Dispatcher {
                     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
                     <!-- css pour les icônes Boostrap -->
                     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
+                    <!-- css Google font -->
+                    <link rel="preconnect" href="https://fonts.googleapis.com">
+                    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+                    <link href="https://fonts.googleapis.com/css2?family=Bricolage+Grotesque:opsz,wght@12..96,200..800&display=swap" rel="stylesheet">
                     <link href="../css/style.css" rel="stylesheet">
                     <!-- Icône personnalisée pour le site -->
                     <link rel="icon" type="image/x-icon" href="../image/favicon.ico">
